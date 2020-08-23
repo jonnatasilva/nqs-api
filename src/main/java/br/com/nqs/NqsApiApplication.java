529d183f77556ede4e927fdb6c27f8390c530bc0
jonnatasilva <jonnatas.silva17@gmail.com>
2020-08-23 01:53:32 -0300

    Estrutura inicial

---
+++ src/main/java/br/com/nqs/NqsApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.nqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NqsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NqsApiApplication.class, args);
+	}
+
+}
